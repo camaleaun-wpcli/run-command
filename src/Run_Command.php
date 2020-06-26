941f790dd3c40b336fc3780de99957e4088abf9b
Gilberto Tavares <gilberto.tavares@envolvelabs.com.br>
2020-06-26 08:15:23 -0300

    Updates

--- src/Run_Command.php
+++ src/Run_Command.php
@@ -8,7 +8,6 @@ namespace Camaleaun;
 
 use WP_CLI;
 use WP_CLI\Utils;
-use Mustangostang\Spyc;
 
 /**
  * Run scripts.
@@ -38,26 +37,75 @@ class Run_Command {
 	 * ## EXAMPLES
 	 *
 	 *     $ wp run install
-	 *     Success: Finish script run.
+	 *     Success: Finish 'install' script.
 	 *
 	 * @when before_wp_load
 	 */
 	public function __invoke( $args, $assoc_args ) {
 		list( $script ) = $args;
-		$extra_config = WP_CLI::get_runner()->extra_config;
-		$scripts      = isset( $extra_config['scripts'] ) ? $extra_config['scripts'] : null;
 
-		if ( ! isset( $scripts ) || ! isset( $scripts[ $script ] ) ) {
+		$configurator = WP_CLI::get_configurator();
+
+		list( $config, $extra_config ) = $configurator->to_array();
+
+		$scripts = Utils\get_flag_value( $extra_config, 'scripts' );
+
+		$commands = Utils\get_flag_value( $scripts, $script );
+
+		if ( ! $commands ) {
 			WP_CLI::error( "Script '$script' not defined." );
 		}
 
-		foreach ( $scripts[ $script ] as $command ) {
+		foreach ( $commands as $command ) {
+			if ( ! preg_match( '/^wp\s+/', $command ) ) {
+				continue;
+			}
 			$command = preg_replace( '/^wp\s+/', '', $command );
-			WP_CLI::log( $command );
-			WP_CLI::runcommand( $command, array( 'return' => 'all' ) );
+
+			$output = WP_CLI::runcommand(
+				$command,
+				array(
+					'return'     => 'all',
+					'launch'     => true,
+					'exit_error' => false,
+				)
+			);
+
+			$quiet = Utils\get_flag_value( $config, 'quiet' );
+			$quiet = $quiet || preg_match( '/\s+--quiet[\s=$]?/', $command );
+
+			$command = preg_replace( '/\s+--quiet=?[^\s$]*/', '', $command );
+
+			if ( ! $quiet ) {
+				WP_CLI::log( WP_CLI::colorize( "Running: %gwp {$command}%n" ) );
+			}
+			$output = $output->stderr ? $output->stderr : $output->stdout;
+			if ( ! empty( $output ) ) {
+				self::output( $output, $quiet );
+			}
+		}
+	}
+
+	private static function output( $output, $quiet = false ) {
+		if ( empty( $output ) ) {
+			return;
+		}
+		$type    = '';
+		$message = $output;
+		preg_match( '/^(Success|Warning|Error)?:?\s*([^$]+)/', $output, $match );
+		if ( 3 === count( $match ) ) {
+			list( $type, $message ) = array_slice( $match, 1 );
 		}
 
-		// WP_CLI::log( 'Test' );
-		// var_dump( $extra_config );
+		$type = strtolower( $type );
+		if ( empty( $type ) ) {
+			WP_CLI::line( $message );
+		} elseif ( ! $quiet ) {
+			if ( $type !== 'error' ) {
+				call_user_func( "WP_CLI::$type", $message );
+			} else {
+				WP_CLI::log( WP_CLI::colorize( '%RError:%n ' . $message ) );
+			}
+		}
 	}
 }
